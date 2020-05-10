646338ec684a0525f6157ffd7278e73512f70e92
Dmitriy Zhyzhko <zfour.apps@gmail.com>
2020-05-10 13:20:08 +0200

    Add comments for FloatingLabelTextField

--- FloatingLabelTextField/Classes/View/FloatingLabelTextField.swift
+++ FloatingLabelTextField/Classes/View/FloatingLabelTextField.swift
@@ -47,12 +47,13 @@ import UIKit.UITextField
     
     @IBInspectable var isSecure: Bool = false
     
-    ///Animation
-    private let animDuration: TimeInterval = 0.3
+    /// Delegate
+    public weak var delegate: FloatingLabelTextFieldDelegate?
+       
+    /// Events listener
+    public var events: [EventListener] = []
     
-    ///Views
-    internal var stateViewConstraintWidth: NSLayoutConstraint?
-    internal var headerLabelConstraintTop: NSLayoutConstraint?
+    /// Views
     internal var headerLabel: AttributedLabel?
     internal var descriptionLabel: AttributedLabel?
     internal var textField: UITextField?
@@ -60,44 +61,50 @@ import UIKit.UITextField
     internal var stateView: UIView?
     internal var extraViewsContainer: UIView?
     
-    ///Letter spacing
+    /// Constraints
+    internal var stateViewConstraintWidth: NSLayoutConstraint?
+    internal var headerLabelConstraintTop: NSLayoutConstraint?
+    
+    /// State view width
+    internal lazy var stateViewWidth: CGFloat = 0
+    
+    /// Animation duration
+    private let animDuration: TimeInterval = 0.3
+    
+    /// Letter spacing
     private lazy var secureTextLetterSpacing: CGFloat = 5
     private lazy var unsecureTextLetterSpacing: CGFloat = -0.6
     
-    ///Text
+    /// Text
     private lazy var unsecureText = String()
     private lazy var secureText = String()
-    
-    ///Size
-    internal lazy var stateViewWidth: CGFloat = 0
-    
-    ///Delegate
-    public weak var delegate: FloatingLabelTextFieldDelegate?
-    
-    ///Events
-    public var events: [EventListener] = []
     
     var isFloatingShown = false
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configure()
+        setUpView()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        configure()
+        setUpView()
     }
 }
 
-// MARK: - Configure
+// MARK: - Setup
 extension FloatingLabelTextField {
     
-    private func configure() {
+    
+    /// Basic configuration and view creation
+    private func setUpView() {
         backgroundColor = .clear
         
+        
+        // Registers all the library fonts
         UIFont.registerLibraryFonts()
         
+        // Creates all the views
         createHeaderLabel()
         createTextField()
         createSeparator()
@@ -105,7 +112,10 @@ extension FloatingLabelTextField {
         createDescriptionLabel()
     }
     
+    
+    /// Creates header label
     private func createHeaderLabel() {
+        // Init and add header as a subview
         headerLabel = AttributedLabel()
         headerLabel?.alpha = 0
         headerLabel?.font = FontFamily.Gerbera.light.font(size: 12)
@@ -113,10 +123,13 @@ extension FloatingLabelTextField {
         headerLabel?.translatesAutoresizingMaskIntoConstraints =  false
         addSubview(headerLabel!)
         
+        // Applies constraints for header label
         constraints(for: headerLabel)
     }
     
+    /// Creates text filed
     private func createTextField() {
+        // Init and add text field as a subview
         textField = UITextField()
         textField?.delegate = self
         textField?.font = FontFamily.Gerbera.medium.font(size: 18)
@@ -126,28 +139,37 @@ extension FloatingLabelTextField {
         textField?.addTarget(self, action: #selector(textFieldDidChange(_:)),for: .editingChanged)
         addSubview(textField!)
         
+        // Applies constraints for text field
         constraints(for: textField)
     }
     
+    /// Creates separator view
     private func createSeparator() {
+        // Init and add separator as a subview
         separatorView = UIView()
         separatorView?.backgroundColor = separatorColor
         separatorView?.translatesAutoresizingMaskIntoConstraints =  false
         addSubview(separatorView!)
         
+        // Applies constraints for separator
         constraints(for: separatorView)
     }
     
+    /// Creates state view
     private func createStateView() {
+        // Init and add state view as a subview
         stateView = UIView()
         stateView?.translatesAutoresizingMaskIntoConstraints =  false
         stateViewWidth = frame.width
         addSubview(stateView!)
         
+        // Applies constraints for state view
         constraints(for: stateView)
     }
     
+    /// Creates description label
     private func createDescriptionLabel() {
+        // Init and add description label as a subview
         descriptionLabel = AttributedLabel()
         descriptionLabel?.sizeToFit()
         descriptionLabel?.textColor = .white
@@ -155,32 +177,49 @@ extension FloatingLabelTextField {
         descriptionLabel?.translatesAutoresizingMaskIntoConstraints =  false
         addSubview(descriptionLabel!)
         
+        // Applies constraints for description label
         constraints(for: descriptionLabel)
     }
+}
+
+// MARK: - Helper methods
+extension FloatingLabelTextField {
     
+    /// Handles inputed text.
+    /// - Parameter text: Relevant text.
     private func handleText(_ text: String?) {
+        // Notifies that text has been changed
         events.forEach { $0(.didTextChanged(self)) }
         
+        // Toggle header state (show or hide)
         if let inputedText = text {
             let symbolsCount = inputedText.count
             if symbolsCount >= 1 && !isFloatingShown { toggleHeaderLabel() }
             else if symbolsCount < 1 { toggleHeaderLabel() }
         }
         
+        // Update secure line depending on text state
         if let delegate = self.delegate {
             let (state, descr, color) = delegate.state(for: self)
             updateSecureLine(to: state, text: descr, color: color)
         }
     }
     
+    /// Toggle header label state
     private func toggleHeaderLabel() {
         guard let headerLabel = headerLabel else { return }
+        
+        // Updates the state
         isFloatingShown = !isFloatingShown
         
+        // Calculate correct header position
         let currFrame = headerLabel.frame
         let value = !isFloatingShown ? currFrame.origin.y + currFrame.height : currFrame.origin.y - currFrame.height
+        
+        // Updates constraint value
         headerLabelConstraintTop?.constant = value
         
+        // Animate view with already changed constraint value
         UIView.animateKeyframes(withDuration: animDuration, delay: 0, options: .calculationModeCubic, animations: {
             
             UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: self.animDuration) {
@@ -194,48 +233,13 @@ extension FloatingLabelTextField {
         }, completion: nil)
     }
     
-    public func switchTextFormat(secure: Bool) {
-        isSecure = !isSecure
-        setAttributedText(text: secure ? secureText : unsecureText)
-    }
     
-    public func updateSecureLine(to state: InputTextState, text: String?, color: UIColor?) {
-        guard let stateView = stateView else { return }
-        
-        stateViewConstraintWidth?.constant = state.rawValue * stateViewWidth
-        if state == .idle {
-            descriptionLabel?.textColor = .white
-            descriptionLabel?.isHidden = true
-            return
-            
-        } else { descriptionLabel?.isHidden = false }
-        
-        if let descrLabel = descriptionLabel {
-            descrLabel.text = text
-            descrLabel.textColor = color
-            stateView.backgroundColor = color
-        }
-    }
-    
-    public func isSecute() -> Bool { return isSecure }
-    
-    public func setKeyboardType(_ type: UIKeyboardType) {
-         textField?.keyboardType = type
-    }
-    
-    public func getText() -> String {
-        return unsecureText
-    }
-    
-    public func setText(_ text: String) {
-        updateText(text: text)
-        setAttributedText(text: isSecure ? secureText : unsecureText)
-        handleText(unsecureText)
-    }
-    
-    private func setAttributedText(text: String) {
+    /// Sets new text and adjusts letter spacing.
+    /// - Parameter text: New text.
+    private func setAttributedText(_ text: String) {
         guard let textField = textField else { return }
         
+        // Update letter spacing depending on text format (secure or not)
         let letterSpacing = isSecure ? secureTextLetterSpacing : unsecureTextLetterSpacing
         let attributedString = NSMutableAttributedString(string: text)
         attributedString.addAttribute(.kern,
@@ -244,17 +248,91 @@ extension FloatingLabelTextField {
         textField.attributedText = attributedString
     }
     
-    private func updateText(text: String) {
+    
+    /// Refreshes current text.
+    /// - Parameter text: New unsecure text.
+    private func refreshSecureText(_ text: String) {
         unsecureText = text
+        // Refreshs secure text
         secureText.removeAll()
+        // Fills string with secure chars
         for _ in unsecureText {  secureText += "●" }
+    }
+}
+
+// MARK: - Configuration
+extension FloatingLabelTextField {
+    
+    /// Switches text format to secure or not.
+    /// - Parameter secure: Represents should text be secure or not .
+    public func switchTextFormat(secure: Bool) {
+       // Toggles secure state
+       isSecure = !isSecure
+       
+       // Updates text with new state
+       setAttributedText(secure ? secureText : unsecureText)
+    }
+       
+    public func updateSecureLine(to state: InputTextState, text: String?, color: UIColor?) {
+        guard let stateView = stateView else { return }
+       
+        // Updates line width depending on state
+        stateViewConstraintWidth?.constant = state.rawValue * stateViewWidth
+       
+        // If it is idle state then hide descr label
+        // otherwise make it visible
+        if state == .idle {
+            descriptionLabel?.textColor = .white
+            descriptionLabel?.isHidden = true
+            return
+        } else {
+            descriptionLabel?.isHidden = false
+        }
+       
+        // Sets new desc text and updates state view color
+        if let descrLabel = descriptionLabel {
+            descrLabel.text = text
+            descrLabel.textColor = color
+            stateView.backgroundColor = color
+        }
+    }
+
+    /// Returns current text state
+    public func isSecute() -> Bool { return isSecure }
+
+    /// Sets keyboard type.
+    /// - Parameter type: UIKeyboardType to be setted.
+    public func setKeyboardType(_ type: UIKeyboardType) {
+        textField?.keyboardType = type
+    }
+
+    /// Get current text in unsecure format
+    public func getText() -> String {
+        return unsecureText
+    }
+    
+    /// Updates current text with the new one.
+    /// - Parameter text: New text.
+    public func setText(_ text: String) {
+       // Refreshes secure text
+       refreshSecureText(text)
+       // Sets new text
+       setAttributedText(isSecure ? secureText : unsecureText)
+       // Handles new text
+       handleText(unsecureText)
     }
 }
 
 // MARK: - Extra views
 extension FloatingLabelTextField {
     
+    
+    /// Add right label to the text field with specific color.
+    /// - Parameters:
+    ///   - text: Label's text.
+    ///   - color: Text color.
     public func addRightExtraView(text: String, color: UIColor) {
+        // Init and button with basic configs
         let button = UIButton(frame: CGRect(x: 0, y: 0,
                                             width: frame.height, height: frame.height))
         button.setTitle(text, for: .normal)
@@ -263,14 +341,22 @@ extension FloatingLabelTextField {
         button.setTitleColor(color.darker(), for: .highlighted)
         button.sizeToFit()
         button.addTarget(self, action: #selector(onExtraViewClick(_:)), for: .touchDown)
+        
+        // Sets button as text field's right view
         textField?.rightView = button
     }
     
+    /// Add images to the right of the text field.
+    /// - Parameters:
+    ///   - images: Array of images.
     public func addRightExtraViews(images: [UIImage]) {
-        let viewHeight: CGFloat = frame.height, viewSpacing: CGFloat = 8
+        let viewSpacing: CGFloat = 8
+        let viewHeight: CGFloat = frame.height
         let containerView = UIView()
-        var i = 0, conainerViewWidth: CGFloat = 0
+        var conainerViewWidth: CGFloat = 0
+        var i = 0
         
+        // Creates buttons in loop and adds to the container
         while i < images.count {
             let image = images[i]
             let spacing = (i != 0 ? viewSpacing : 0)
@@ -288,18 +374,27 @@ extension FloatingLabelTextField {
             i = i + 1
         }
         
+        // Updates container view size
         containerView.frame = CGRect(x: 0, y: 0, width: conainerViewWidth, height: viewHeight)
+        
+        // Sets button as text field's right view
         textField?.rightView = containerView
     }
     
+    
+    /// Handles click on extra view.
+    /// - Parameter button: Button that has been clicked.
     @objc private func onExtraViewClick(_ button: UIButton) {
+        // Gets tag that represent button's index in the parent view
         let index = button.tag
         if let rightView = textField?.rightView {
             if let button = rightView as? UIButton {
+                // Notifies for button pressed event
                 events.forEach { $0(.didExtraButtonPressed(floatingTextField: self, index: index, button: button)) }
                 
             } else if let subviews = textField?.rightView?.subviews, subviews.count > 0,
                 let button = subviews[index] as? UIButton {
+                // Notifies for button pressed event
                 events.forEach { $0(.didExtraButtonPressed(floatingTextField: self, index: index, button: button)) }
             }
         }
@@ -310,30 +405,45 @@ extension FloatingLabelTextField {
 extension FloatingLabelTextField: UITextFieldDelegate {
     
     public func textFieldDidEndEditing(_ textField: UITextField) {
+        // Notifies that editing has been finised
         events.forEach { $0(.didEndEditing(self)) }
     }
     
     public func textFieldDidBeginEditing(_ textField: UITextField) {
+        // Notifies that editing has been started
         events.forEach { $0(.didStartEditing(self)) }
     }
     
     public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange,
                           replacementString string: String) -> Bool {
+        // If we should not hide inputed text then return immediately
         guard isSecure else { return true }
         
+        // Creates new instance of secure text
         var secureText = String()
         let offsetToUpdate = unsecureText.index(unsecureText.startIndex, offsetBy: range.location)
         
+        // Refreshes secure text
         if string.isEmpty {
             unsecureText.remove(at: offsetToUpdate)
             return true
-        } else { unsecureText.insert(string.first!, at: offsetToUpdate) }
+        } else {
+            unsecureText.insert(string.first!, at: offsetToUpdate)
+        }
         
+        // Fills string with secure chars
         for _ in unsecureText {  secureText += "●" }
+        
+        // Save secure text
         self.secureText = secureText
         
-        setAttributedText(text: secureText)
+        // Update text with secure onte
+        setAttributedText(secureText)
+        
+        // Handle inputed text
         handleText(unsecureText)
+        
+        // Notifies that text has been changed
         NotificationCenter.default.post(name: UITextField.textDidChangeNotification, object: textField)
         
         return false
@@ -341,8 +451,12 @@ extension FloatingLabelTextField: UITextFieldDelegate {
     
     @objc private func textFieldDidChange(_ textField: UITextField) {
         guard let text = textField.text else { return }
-        if !isSecure { updateText(text: text) }
+        
+        // Refreshes secure text if needed
+        if !isSecure { refreshSecureText(text) }
         else if !secureText.isEmpty { secureText.removeLast() }
+        
+        // Handle new text
         handleText(text)
     }
 }
