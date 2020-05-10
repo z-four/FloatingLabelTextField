44f72de0f05f57feb54c07d42fc5e39eac867ad2
Dmitriy Zhyzhko <zfour.apps@gmail.com>
2020-05-10 12:33:40 +0200

    AttributedLabel minor refactoring

--- FloatingLabelTextField/Classes/View/AttributedLabel.swift
+++ FloatingLabelTextField/Classes/View/AttributedLabel.swift
@@ -22,11 +22,15 @@ public final class AttributedLabel: UILabel {
                 attributedString = NSMutableAttributedString(string: text ?? "")
                 text = nil
             }
-            attributedString.addAttribute(.kern, value: newValue, range: NSRange(location: 0, length: attributedString.length))
+            attributedString.addAttribute(.kern,
+                                          value: newValue,
+                                          range: NSRange(location: 0, length: attributedString.length))
             attributedText = attributedString
         }
         get {
-            if let currentLetterSpace = attributedText?.attribute(NSAttributedString.Key.kern, at: 0, effectiveRange: .none) as? CGFloat {
+            if let currentLetterSpace = attributedText?.attribute(NSAttributedString.Key.kern,
+                                                                  at: 0,
+                                                                  effectiveRange: .none) as? CGFloat {
                 return currentLetterSpace
             }
             else { return 0 }
@@ -47,23 +51,32 @@ public final class AttributedLabel: UILabel {
 // MARK: - Configure
 extension AttributedLabel {
     
+    
+    /// Configure attributed string with default params
     private func configure() {
         let attrString = NSMutableAttributedString(string: "")
-        attrString.addAttribute(NSAttributedString.Key.font, value: self.font, range: NSMakeRange(0, attrString.length))
+        attrString.addAttribute(NSAttributedString.Key.font,
+                                value: self.font ?? FontFamily.Gerbera.light,
+                                range: NSMakeRange(0, attrString.length))
         self.attributedText = attrString
+    }
+}
+
+
+// MARK: - Getters & setters
+extension AttributedLabel {
+      
+    func setText(text: String?, color: UIColor = .white) {
+        guard let text = text else { return }
+        self.text = text
+          
+        let attributes = self.attributedText?.attributes(at: 0, effectiveRange: nil)
+        let attr = NSMutableAttributedString.init(string: text, attributes: attributes)
+        attr.addAttributes([.foregroundColor : color], range: NSRange(text.range(of: text)!, in: text))
+        self.attributedText = attr
     }
     
     func getText() -> String? {
         return self.attributedText?.string
-    }
-    
-    func setText(text: String?, color: UIColor = .white) {
-        guard let text = text else { return }
-        self.text = text
-        
-        let attributes = self.attributedText?.attributes(at: 0, effectiveRange: nil)
-        let attr = NSMutableAttributedString.init(string: text, attributes: attributes)
-        attr.addAttributes( [.foregroundColor : color], range: NSRange(text.range(of: text)!, in: text))
-        self.attributedText = attr
     }
 }
